ef0a69cc61b0bfc81c754d67668bc1ea9a378029
Mike Parker <mikeparker@crescerance.com>
2019-08-18 13:21:53 -0400

    randomizingrolls

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,28 +16,68 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  DicePage({Key key}) : super(key: key);
+
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  final List<String> _allDiceAssets = [
+    'images/dice1.png',
+    'images/dice2.png',
+    'images/dice3.png',
+    'images/dice4.png',
+    'images/dice5.png',
+    'images/dice6.png',
+  ];
+  int leftDiceStateIndex = 0;
+  int rightDiceStateIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    print('I was called');
     return Container(
       child: Center(
         child: Row(
           children: <Widget>[
             Expanded(
               child: FlatButton(
-                onPressed: () => print('Left Dice Clicked'),
-                child: Image.asset('images/dice1.png'),
+                onPressed: () => rollAllDice(),
+                child: Image.asset(_allDiceAssets[leftDiceStateIndex]),
               ),
             ),
             Expanded(
               child: FlatButton(
-                onPressed: () => print('Right Dice Clicked'),
-                child: Image.asset('images/dice6.png'),
+                onPressed: () => rollAllDice(),
+                child: Image.asset(_allDiceAssets[rightDiceStateIndex]),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void rollOneDice(String chosenDice) {
+    int newRoll = Random().nextInt(6);
+    switch (chosenDice.toUpperCase()) {
+      case "LEFT":
+        setState(() {
+          leftDiceStateIndex = newRoll;
+        });
+        break;
+      case "RIGHT":
+        setState(() {
+          rightDiceStateIndex = newRoll;
+        });
+        break;
+      default:
+    }
+  }
+
+  void rollAllDice() {
+    rollOneDice('LEFT');
+    rollOneDice('RIGHT');
   }
 }
