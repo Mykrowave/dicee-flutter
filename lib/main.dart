42b52ac1fc2d8577cee7437775f14e336b513641
Mike Parker <mikeparker@crescerance.com>
2019-08-18 12:02:20 -0400

    diceexpanded

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,19 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+          Expanded(
+            child: Image.asset('images/dice6.png'),
+          ),
+        ],
+      ),
+    );
   }
 }
